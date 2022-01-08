e63212c77fcd69adcb5ae98d23f82b1b53c63f5c
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-01-07 16:42:52 -0800

    Use zod over env-schema

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,8 +1,7 @@
-import envSchema from "env-schema";
-import S from "fluent-json-schema";
+import { z } from "zod";
 
-export const schema = S.object()
-  .prop("HOST", S.string().default("127.0.0.1"))
-  .prop("PORT", S.number().required());
-
-export const CONFIG = envSchema({ schema }) as { HOST: string; PORT: number };
+const schema = z.object({
+  HOST: z.string(),
+  PORT: z.number(),
+});
+export const CONFIG = schema.parse(process.env);
