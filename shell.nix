27e1e8cf5629f3996484fc354bebe35b14f3ae67
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-01-07 16:40:08 -0800

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,6 @@
+{ pkgs ? import <nixpkgs> { } }:
+let nodeVersion = pkgs.nodejs-17_x;
+in pkgs.mkShell {
+  nativeBuildInputs =
+    [ nodeVersion (pkgs.yarn.override { nodejs = nodeVersion; }) ];
+}
